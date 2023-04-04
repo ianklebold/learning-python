30be1ffb7e155fb5a11fa2c707ed26158379a8f6
ianklebold <ianstgo@gmail.com>
2023-04-04 17:26:44 -0300

    Avance 1

---
+++ primerproyecto/String_section.py
@@ -0,0 +1,9 @@
+print("Hello"+' '+'Ian')
+print("My name is Ian")
+print("My name is \"Ian\"")
+
+
+print("This is a line \nand this is another line")
+print("\tThis is a third line")
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+print("Barra Normal: /\nBarra Invertida: \\")
