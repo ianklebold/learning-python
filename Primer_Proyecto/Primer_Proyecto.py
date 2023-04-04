808c3c7c7a402441cc23dd4b44f6e522835141d9
ianklebold <ianstgo@gmail.com>
2023-04-04 09:23:16 -0300

    first commit

---
+++ Primer_Proyecto/Primer_Proyecto.py
@@ -0,0 +1,5 @@
+print("Hello World")
+print("Hello World in spanish is 'Hola'") #El 'Hola' lo considera como texto dentro de otro texto
+print(200)
+print(200 + 100)
+print("200"+"100") #Esto es concatenacion de strings
